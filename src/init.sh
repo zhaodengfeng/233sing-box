39214d29c9801eb17d0fcd5cf30d4584bf6a6189
zhaodengfeng <zhaodengfeng@gmail.com>
2026-03-12 16:20:49 +0800

    fix: point installer update source to fork repo

--- src/init.sh
+++ src/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-author=233boy
-# github=https://github.com/233boy/sing-box
+author=zhaodengfeng
+# github=https://github.com/zhaodengfeng/233sing-box
 
 # bash fonts colors
 red='\e[31m'
@@ -83,7 +83,7 @@ is_conf_dir=$is_core_dir/conf
 is_log_dir=/var/log/$is_core
 is_sh_bin=/usr/local/bin/$is_core
 is_sh_dir=$is_core_dir/sh
-is_sh_repo=$author/$is_core
+is_sh_repo=zhaodengfeng/233sing-box
 is_pkg="wget unzip tar qrencode"
 is_config_json=$is_core_dir/config.json
 is_caddy_bin=/usr/local/bin/caddy
